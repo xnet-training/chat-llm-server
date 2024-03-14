c8f02175f4c803bb678bbf5d7c35b3fdafe15167
ianache <ianache@comsatel.com.pe>
2024-03-14 14:08:29 +0000

    fix: referencia de clases

---
+++ src/main/java/com/crossnetcorp/chat/llmserver/LlmServerImpl.java
@@ -0,0 +1,8 @@
+package com.crossnetcorp.chat.llmserver;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+public class LlmServerImpl extends LLMChatServiceGrpc.LLMChatServiceImplBase {
+
+}
---
+++ src/main/java/com/crossnetcorp/chat/llmserver/MainApp.java
@@ -0,0 +1,22 @@
+package com.crossnetcorp.chat.llmserver;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import java.io.IOException;
+
+
+public class MainApp {
+    public static void main(String[] args) {
+        try {
+            Server server = ServerBuilder.forPort(8999).addService(new LlmServerImpl()).build();
+
+            server.start();
+            System.out.println("Server started at " + server.getPort());
+            server.awaitTermination();
+        } catch (IOException e) {
+            System.out.println("Error: " + e);
+        } catch (InterruptedException e) {
+            System.out.println("Error: " + e);
+        }
+    }
+}
